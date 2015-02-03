849ef716e7290f02ca74091059b80344a1fcf863
zense10827 <5510613424@student.tu.ac.th>
2015-02-03 05:58:44 +0000

    finish

--- lib/part2.rb
+++ lib/part2.rb
@@ -7,15 +7,18 @@ end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
-  if s[0]==s[/[aeiou]/]
+  if !/[[:alpha:]]/.match(s[0])
     return false
   end
-  return true
+  if !s.start_with?("a","A","e","E","i","I","o","O","u","U") and !s.empty?
+    return true
+  end
+  return false
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
-  if ! /\d/ .match(s) or !/[0-1]/ .match(s)
+  if !/[[:digit:]]/.match(s) or !/[0-1]/.match(s) or /[[:alpha:]]/.match(s)
     return false
   end
   a = s.to_i(2)
